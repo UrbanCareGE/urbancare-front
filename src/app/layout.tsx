023b17fa260fcda12fa6835189c28457a1b3784d
leogogichaishvili <lgogi19@freeuni.edu.ge>
2025-10-05 17:39:09 +0400

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,12 @@ import "./globals.scss";
 import React from "react";
 import ReactQueryProvider from "@/components/provider/provider/ReactQueryProvider";
 import MyThemeProvider from "@/components/common/provider/MyThemeProvider";
-import {Inter} from "@next/font/google";
 
 export const metadata: Metadata = {
     title: "urbancare",
     description: "urbancare",
 };
 
-const inter = Inter({subsets: ["latin"], variable: "--font-inter"});
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
@@ -18,10 +16,9 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             lang="en"
             style={{colorScheme: ""}}
             suppressHydrationWarning
-            className={inter.variable}
         >
         <ReactQueryProvider>
-            <body className="h-screen w-full flex flex-col font-sans antialiased" suppressHydrationWarning>
+            <body className="h-screen w-full flex flex-col antialiased" suppressHydrationWarning>
             <MyThemeProvider>
                 {children}
             </MyThemeProvider>
