79169269bfd8844c70bfdae51a03a4b64beae520
Afaqa <laphakidze@dga.gov.ge>
2025-10-26 19:03:09 +0400

    urgents fix

---
+++ src/app/(home)/urgent/UrgentList.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import {UrgentService} from "@/service/urgent-service";
+import UrgentCard from "@/app/(home)/urgent/UrgentCard";
+import {useAuth} from "@/components/provider/AuthProvider";
+import {useQuery} from "@tanstack/react-query";
+import Page from "@/app/(home)/urgent/page";
+import {Leapfrog} from "ldrs/react";
+import {cn} from "@/lib/utils";
+import {Basic} from "@/app/layout";
+
+import 'ldrs/react/Leapfrog.css'
+
+
+const UrgentList = () => {
+    const {user, isLoading: isUserLoading} = useAuth();
+
+    const {data, isError, isLoading} = useQuery({
+        queryKey: ["urgent_list", user?.selectedApartment?.id],
+        queryFn: () => UrgentService.getUrgentList(user?.selectedApartment?.id ?? 'yle'),
+        enabled: !!user?.selectedApartment?.id,
+        staleTime: 5 * 1000,
+    });
+
+    if (isUserLoading) {
+        return <ListLoader/>;
+    }
+
+    if (!user?.selectedApartment?.id) {
+        return (
+            <div className="flex items-center justify-center p-4">
+                No apartment selected
+            </div>
+        );
+    }
+
+    return (
+        <ul className="flex flex-col gap-3 py-3">
+            {isLoading && <ListLoader/>}
+
+            {isError && (
+                <div className="flex items-center justify-center p-4 text-red-500">
+                    Error loading urgent items
+                </div>
+            )}
+
+            {data?.map((item) => (
+                <UrgentCard key={item.id} {...item}/>
+            ))}
+        </ul>
+    );
+};
+
+
+const ListLoader = ({className}: Basic) => {
+    return (
+        <div className={cn("z-60 flex w-full h-14 justify-center items-center fixed top-[70%]", className)}>
+            <Leapfrog
+                size="40"
+                speed="1.75"
+                color="#02c2c5"
+            />
+        </div>
+    );
+};
+
+export default UrgentList;
--- src/app/(home)/urgent/page.tsx
+++ src/app/(home)/urgent/page.tsx
@@ -1,78 +1,20 @@
 'use client'
 
-import React, {Suspense} from "react";
-import {UrgentService} from "@/service/urgent-service";
-import UrgentCard from "@/app/(home)/urgent/UrgentCard";
-import {Leapfrog} from 'ldrs/react'
-import 'ldrs/react/Leapfrog.css'
+import React from "react";
 import AddUrgent from "@/app/(home)/urgent/AddUrgent";
-import {Basic} from "@/app/layout";
-import {cn} from "@/lib/utils";
-import {useAuth} from "@/components/provider/AuthProvider";
-import {useQuery} from "@tanstack/react-query";
+import UrgentList from "@/app/(home)/urgent/UrgentList";
 
 
 const Page = () => {
     return (
         <div className={"bg-white w-full flex flex-col"}>
             <AddUrgent/>
-            <Suspense fallback={<ListLoader/>}>
-                <UrgentList/>
-            </Suspense>
+            <UrgentList/>
         </div>
     )
 }
 
-const ListLoader = ({className}: Basic) => {
-    return (
-        <div className={cn("z-60 flex w-full h-14 justify-center items-center fixed top-[70%]", className)}>
-            <Leapfrog
-                size="40"
-                speed="1.75"
-                color="#02c2c5"
-            />
-        </div>
-    );
-};
 
-const UrgentList = () => {
-    const {user, isLoading: isUserLoading} = useAuth();
-
-    const {data, isError, isLoading} = useQuery({
-        queryKey: ["urgent_list", user?.selectedApartment?.id],
-        queryFn: () => UrgentService.getUrgentList(user?.selectedApartment?.id ?? 'yle'),
-        enabled: !!user?.selectedApartment?.id,
-        staleTime: 5 * 1000,
-    });
-
-    if (isUserLoading) {
-        return <ListLoader/>;
-    }
-
-    if (!user?.selectedApartment?.id) {
-        return (
-            <div className="flex items-center justify-center p-4">
-                No apartment selected
-            </div>
-        );
-    }
-
-    return (
-        <ul className="flex flex-col gap-3 py-3">
-            {isLoading && <ListLoader/>}
-
-            {isError && (
-                <div className="flex items-center justify-center p-4 text-red-500">
-                    Error loading urgent items
-                </div>
-            )}
-
-            {data?.map((item) => (
-                <UrgentCard key={item.id} {...item}/>
-            ))}
-        </ul>
-    );
-};
 export default Page;
 
 
