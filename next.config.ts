9cbe9390eb74800910e16adde9b6042cdb3ea742
leogogichaishvili <lgogi19@freeuni.edu.ge>
2026-02-18 20:26:33 +0400

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true, // Disable to prevent double animations in dev
-    images: {
-        loader: 'custom',
-        loaderFile: './src/lib/image-loader.ts',
-    },
-    allowedDevOrigins: ['shanna-unidentical-mechelle.ngrok-free.dev', 'http://192.168.100.5:3000', 'http://localhost:3000']
+  reactStrictMode: true, // Disable to prevent double animations in dev
+  images: {
+    loader: 'custom',
+    loaderFile: './src/lib/image-loader.ts',
+  },
+  allowedDevOrigins: [
+    '*.ngrok-free.app',
+    'urbancare.ge',
+    'http://localhost:3000',
+  ],
 };
 
 export default nextConfig;
