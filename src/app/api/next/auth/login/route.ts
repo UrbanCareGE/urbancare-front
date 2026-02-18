bb1de97b5398e3975c8461d39f3b2df85309651b
leogogichaishvili <lgogi19@freeuni.edu.ge>
2026-02-18 20:43:36 +0400

    minor changes

--- src/app/api/next/auth/login/route.ts
+++ src/app/api/next/auth/login/route.ts
@@ -1,6 +1,7 @@
 // app/api/auth/login/route.ts
 
 import { AuthService } from '@/service/auth-service';
+import { cookies } from 'next/headers';
 
 export async function POST(request: Request) {
   try {
@@ -14,16 +15,18 @@ export async function POST(request: Request) {
       return Response.json({ error: 'No token received' }, { status: 500 });
     }
 
-    const jsonResponse = Response.json(data);
+    const cookieStore = await cookies();
+    cookieStore.set('auth-token', authToken, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+    });
 
-    jsonResponse.headers.set(
-      'Set-Cookie',
-      `auth-token=${authToken}; Path=/; Max-Age=${60 * 60 * 24 * 7}; HttpOnly; SameSite=Lax`
-    );
-
-    return jsonResponse;
+    return Response.json(data);
   } catch (error) {
-    console.log('Login error:', error);
+    console.error('Login error:', error);
     return Response.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
